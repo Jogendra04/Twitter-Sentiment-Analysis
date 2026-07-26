580fd2bda75d315b194fb90101802c1318434041
Jogendra04 <jogendra0404@gmail.com>
2026-07-26 10:58:45 +0530

    Update frontend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://twitter-sentiment-analysis-t9vn.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
